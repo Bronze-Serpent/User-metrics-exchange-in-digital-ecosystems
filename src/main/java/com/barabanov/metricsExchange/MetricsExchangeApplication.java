1f52e65ceb017bbf8fb14a6e6f262ea3517f0df5
sequ3nce <prbar002@gmail.com>
2026-05-02 12:07:49 +0300

    init

---
+++ src/main/java/com/barabanov/metricsExchange/MetricsExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.barabanov.metricsExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barabanov/metricsExchange/MetricsExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barabanov.metricsExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
